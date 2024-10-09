ce6b4ca8e8bfa4bde545b742c4b66480a500d9a0
Juli-Cavalcanti <julianacavalcanti.dev@gmail.com>
2024-10-09 17:52:08 -0300

    incluindo função

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 let nomeDoHeroi = "Julia";
-let vitorias = 100;
-let derrotas = 0;
-let saldoVitorias = vitorias-derrotas;
-let nivel;
+let vitorias = 110;
+let derrotas = 10;
+let saldoVitorias = vitorias - derrotas;
 
-if(vitorias > 10){
-    nivel = "Ferro";
-}else if(vitorias >= 11 && vitorias <= 20){
-    nivel = "Bronze";
-}else if(vitorias >= 21 && vitorias <= 50){
-    nivel = "Prata"
-}else if(vitorias >= 51 && vitorias <= 80){
-    nivel = "Ouro";
-}else if (vitorias >= 81 && vitorias <= 90){
-    nivel = "Diamante";
-}else if (vitorias >= 91 && vitorias <= 100){
-    nivel = "Lendário";
-}else{
-    nivel = "Imortal";
+function calcularNivel(vitorias) {
+    if (vitorias <= 10) {
+        return "Ferro";
+    } else if (vitorias >= 11 && vitorias <= 20) {
+        return "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        return "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        return "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        return "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        return "Lendário";
+    } else {
+        return "Imortal";
+    }
 }
+
+// Chamada da função e armazenamento do resultado
+let nivel = calcularNivel(saldoVitorias);
 
 console.log(`${nomeDoHeroi} tem saldo de ${saldoVitorias} e está no nível de ${nivel}`);
